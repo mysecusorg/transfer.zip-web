be26125364af75530a386ca52760052ef2c8c8dc
Nate Schmitt <23264669+mysecus@users.noreply.github.com>
2025-04-13 22:03:40 -0500

    Update SelfHostHeader.js

--- frontend/src/components/selfhost/SelfHostHeader.js
+++ frontend/src/components/selfhost/SelfHostHeader.js
@@ -123,50 +123,8 @@ export default function SelfHostHeader() {
               <span className="sr-only">Close menu</span>
               <BIcon name={"x-lg"} aria-hidden="true" className="size-6" />
             </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <Disclosure as="div" className="-mx-3">
-                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                    Tools
-                    <BIcon aria-hidden="true" className="size-5 flex-none group-data-[open]:rotate-180" />
-                  </DisclosureButton>
-                  <DisclosurePanel className="mt-2 space-y-2">
-                    {[...products, ...callsToAction].map((item) => (
-                      <DisclosureButton
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className="block rounded-lg py-2 pl-6 pr-3 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
-                      >
-                        {item.name}
-                      </DisclosureButton>
-                    ))}
-                  </DisclosurePanel>
-                </Disclosure>
-                <Link
-                  to="/#pricing"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Pricing
-                </Link>
-                {/* <Link
-                  to="/explore"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Explore
-                </Link> */}
-                <Link
-                  to="/legal/privacy-policy"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Privacy
-                </Link>
-              </div>
-            </div>
-          </div>
-        </DialogPanel>
+	  </div>
+	  </DialogPanel>
       </Dialog>
     </header>
   )
